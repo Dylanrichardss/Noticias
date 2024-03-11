cfc9f1c5c6be2bfa75a8b0b5a20c4cf87db89064
Dylan Richards <dylan.costa.richards@gmail.com>
2024-03-11 20:53:51 -0300

    Busca noticias

--- noticias.py
+++ noticias.py
@@ -1,23 +1,23 @@
 from GoogleNews import GoogleNews
 from time import sleep
+while True:
+    print('-'*12,'Notícias','-'*12)
+    palavra = input('Digite uma palavra chave: ')
+    print()
+    noticia = GoogleNews(lang='pt-BR')
+    noticia.search(palavra)
 
-print('-'*12,'Notícias','-'*12)
-palavra = input('Digite uma palavra chave: ')
-print()
-noticia = GoogleNews(lang='pt-BR')
-noticia.search(palavra)
+    for i in range(3):
+        sleep(1)
+        if noticia.results()[i]["media"] == 'Poder360':
+            print(noticia.results()[i+3]['media'].upper())
+            print(f'{noticia.results()[i+3]["title"]} - {noticia.results()[i+3]["date"]}')
+            print(noticia.results()[i+3]['link'])
+            print(noticia.results()[i+3]['desc'])
+        else:
+            print(noticia.results()[i]['media'].upper())
+            print(f'{noticia.results()[i]["title"]} - {noticia.results()[i]["date"]}')
+            print(noticia.results()[i]['link'])
+            print(noticia.results()[i]['desc'])
 
-for i in range(3):
-    sleep(1)
-    if noticia.results()[i]["media"] == 'Poder360':
-        print(noticia.results()[i+3]['media'].upper())
-        print(f'{noticia.results()[i+3]["title"]} - {noticia.results()[i+3]["date"]}')
-        print(noticia.results()[i+3]['link'])
-        print(noticia.results()[i+3]['desc'])
-    else:
-        print(noticia.results()[i]['media'].upper())
-        print(f'{noticia.results()[i]["title"]} - {noticia.results()[i]["date"]}')
-        print(noticia.results()[i]['link'])
-        print(noticia.results()[i]['desc'])
-
-input('Pressione qualquer tecla para sair')
+    input('Pressione qualquer tecla para sair')
