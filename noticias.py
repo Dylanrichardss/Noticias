64b74f676a3e9c2135a268832da54345d40fbfa9
Dylan Richards <dylan.costa.richards@gmail.com>
2024-12-26 21:13:27 -0300

    Create README.md

--- noticias.py
+++ noticias.py
@@ -1,5 +1,7 @@
 from GoogleNews import GoogleNews
 from time import sleep
+import os
+
 while True:
     print('-'*12,'Notícias','-'*12)
     palavra = input('Digite uma palavra chave: ')
@@ -9,15 +11,12 @@ while True:
 
     for i in range(3):
         sleep(1)
-        if noticia.results()[i]["media"] == 'Poder360':
-            print(noticia.results()[i+3]['media'].upper())
-            print(f'{noticia.results()[i+3]["title"]} - {noticia.results()[i+3]["date"]}')
-            print(noticia.results()[i+3]['link'])
-            print(noticia.results()[i+3]['desc'])
-        else:
-            print(noticia.results()[i]['media'].upper())
-            print(f'{noticia.results()[i]["title"]} - {noticia.results()[i]["date"]}')
-            print(noticia.results()[i]['link'])
-            print(noticia.results()[i]['desc'])
+        print()
+        print(noticia.results()[i]['media'].upper())
+        print(f'{noticia.results()[i]["title"]} - {noticia.results()[i]["date"]}')
+        print(noticia.results()[i]['link'])
+        print(noticia.results()[i]['desc'])
 
-    input('Pressione qualquer tecla para sair')
+    print()
+    input('Tecle Enter para repetir')
+    os.system('cls')
